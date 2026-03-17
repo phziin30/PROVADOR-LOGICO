1b1b3301db57998f77e7c37608e66d197972a6e2
phziin30 <paulohenriquesousaramos1@gmail.com>
2026-03-17 06:39:58 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 function analiseLexica(formula) {
     const regex = /^[A-Z\s\(\)\~\^\v\→\↔]+$/;
